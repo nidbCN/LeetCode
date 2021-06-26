a106718945a4b3fb5d1a85d4a21fd9bc9489ed97
Gaein nidb <Gaein_nidb_CN@outlook.com>
2021-06-26 23:21:45 +0800

    Add TwoSum

---
+++ TwoSum/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var otherNumber = target - nums[i];
+
+                if (dict.TryGetValue(otherNumber, out var index))
+                    return new int[2] { index, i };
+            
+                dict.Add(nums[i], i);
+            }
+
+            return new int[2];
+        }
+    }
+}
