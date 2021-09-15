9a2d70c9d81420af0e7abfd648bc47a97ec946b9
Gaein_nidb <mail@gaein.cn>
2021-09-15 21:39:51 +0800

    update

---
+++ 9-PalindromeNumber/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            var stack = new Stack<int>();
+
+            while (x != 0)
+            {
+                var num = x % 10;
+
+                if(!stack.TryPeek(out var top))
+                {
+                    if(top != num)
+                    {
+
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+                x /= 10;
+            }
+        }
+    }
+}
