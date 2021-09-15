4e3eda2c230728df31281b4645fee11ae8ada602
Gaein nidb <mail@gaein.cn>
2021-09-15 19:01:32 +0800

    Add InorderTraversal

--- BinaryTreeInorderTraversal/Solution.cs
+++ BinaryTreeInorderTraversal/Solution.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace BinaryTreeInorderTraversal
 {
     public class Solution
     {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root is null) return new List<int>();
 
+            var result = new List<int>();
+
+            result.AddRange(InorderTraversal(root.left));
+
+            result.Add(root.val);
+
+            result.AddRange(InorderTraversal(root.right));
+
+            return result;
+        }
     }
 }
