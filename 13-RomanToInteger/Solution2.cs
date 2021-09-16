35e18f48cc86fd04486004332ed5f9b808a7d841
Gaein nidb <mail@gaein.cn>
2021-09-16 19:01:15 +0800

    Add solution 2

---
+++ 13-RomanToInteger/Solution2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RomanToInteger
+{
+    class Solution2
+    {
+        public int RomanToInt(string s)
+        {
+            var strSpan = s.AsSpan();
+
+            var result = 0;
+            var lastNum = GetRomanCharValue(strSpan[0]);
+
+            foreach(var ch in  strSpan[1..])
+            {
+                var num = GetRomanCharValue(ch);
+
+                if (lastNum < num)
+                {
+                    result -= lastNum;
+                }
+                else
+                {
+                    result += lastNum;
+                }
+
+                lastNum = num;
+            }
+
+            return result + lastNum;
+        }
+
+        private static int GetRomanCharValue(char ch)
+            => ch switch
+            {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => throw new ArgumentException($"Unknow roman char {ch}"),
+            };
+    }
+}
